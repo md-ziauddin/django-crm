50b00078728fa46cdde0bf1c8e0cad430e2a7268
MD Ziauddin <md.ziauddin02@gmail.com>
2023-11-02 13:20:45 +0530

    record view and route implemented

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -52,3 +52,13 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be login to view that page')
+        return redirect('home')
